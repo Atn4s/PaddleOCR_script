845a1600e57fccaf571f9e3527c4ee1c79b4d3b1
João Paulo <61942303+Atn4s@users.noreply.github.com>
2025-05-05 21:49:01 -0300

    Update paddle_install.sh
    versão testada e funcional. teste no: Ubuntu em WSL2

--- paddle_install.sh
+++ paddle_install.sh
@@ -1,3 +1,6 @@
+# instala python3.12-venv e libgomp1 caso não tenha instalado.
+sudo apt install python3.12-venv libgomp1
+
 # inicia ambiente python
 python3 -m venv PaddleOCR
 
@@ -11,16 +14,13 @@ pip install paddlepaddle
 pip install paddlepaddle-gpu
 
 # pip install
-pip install "paddleocr>=2.0.6" # 2.0.6 version is recommended
+pip install paddleocr
 
-#build
-pip install build
+# instalação dependencias
+pip install setuptools
 
-# build .whl
-python3 -m build
-
-# verficar versão Paddle
-paddleocr -h
+# garantir que ambiente está configurado
+pip install -U pip setuptools wheel
 
 # Uso Paddle:
 paddleocr --image_dir doc/imgs_en/254.jpg --lang=pt # suporta en também no idioma
